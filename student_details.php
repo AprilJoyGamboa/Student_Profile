ca8bd95adc027536fe37b9ab1b22e603e95b027b
meow <202180369@psu.palawan.edu.ph>
2023-11-19 18:06:12 +0800

    fixed student_details.php

--- student_details.php
+++ student_details.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once("db.php"); // Include the file with the Database class
 
 class StudentDetails {
@@ -12,7 +13,8 @@ class StudentDetails {
     public function create($data) {
         try {
             // Prepare the SQL INSERT statement
-            $sql = "INSERT INTO student_details(student_id, contact_number, street, zip_code, town_city, province) VALUES(:student_id, :contact_number, :street, :zip_code, :town_city,:province);";
+            $sql = "INSERT INTO student_details(student_id, contact_number, street, zip_code, town_city, province) 
+                    VALUES(:student_id, :contact_number, :street, :zip_code, :town_city, :province)";
             $stmt = $this->db->getConnection()->prepare($sql);
 
             // Bind values to placeholders
@@ -30,14 +32,60 @@ class StudentDetails {
             return $stmt->rowCount() > 0;
 
         } catch (PDOException $e) {
-            // Handle any potential errors here
-            echo "Error: " . $e->getMessage();
-            throw $e; // Re-throw the exception for higher-level handling
+            // Log and re-throw the exception for higher-level handling
+            error_log("Error creating student details: " . $e->getMessage());
+            throw $e;
         }
-        
     }
 
-    // Other CRUD methods for student details
-}
+    // Retrieve student details based on student ID
+    public function read($student_id) {
+        try {
+            $sql = "SELECT * FROM student_details WHERE student_id = :student_id";
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->bindParam(':student_id', $student_id);
+            $stmt->execute();
 
+            // Fetch the student data as an associative array
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            // Log and re-throw the exception for higher-level handling
+            error_log("Error reading student details: " . $e->getMessage());
+            throw $e;
+        }
+    }
+
+    // Update student details based on student ID
+    public function update($student_id, $data) {
+        try {
+            $sql = "UPDATE student_details SET
+                    contact_number = :contact_number,
+                    street = :street,
+                    town_city = :town_city,
+                    province = :province,
+                    zip_code = :zip_code
+                    WHERE student_id = :student_id";
+
+            $stmt = $this->db->getConnection()->prepare($sql);
+            // Bind parameters
+            $stmt->bindParam(':student_id', $student_id);
+            $stmt->bindParam(':contact_number', $data['contact_number']);
+            $stmt->bindParam(':street', $data['street']);
+            $stmt->bindParam(':town_city', $data['town_city']);
+            $stmt->bindParam(':province', $data['province']);
+            $stmt->bindParam(':zip_code', $data['zip_code']);
+
+            // Execute the query
+            $stmt->execute();
+
+            return $stmt->rowCount() > 0;
+
+        } catch (PDOException $e) {
+            // Log and re-throw the exception for higher-level handling
+            error_log("Error updating student details: " . $e->getMessage());
+            throw $e;
+        }
+    }
+}
 ?>
